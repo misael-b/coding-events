0791f90fe06c0a26eef6f91edaeb88b108640882
mbarcenas <misael.barcenas@yahoo.com>
2024-01-24 19:09:26 -0600

    completed book examples for validation

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -20,13 +22,17 @@ public class EventController {
     }
 
     @GetMapping("create") // lives at /events/create
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute(new Event());
         return "events/create";
     }
 
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+        if (errors.hasErrors()){
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,18 +1,32 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
+    @Size(min = 3, max=50, message = "Name must be between 3 and 50 characters!")
+    @NotBlank
     private String name;
+    @Size(max=500, message = "Description too long.")
     private String description;
+    @NotBlank
+    @Email(message = "Invalid Email. Try again")
+    private String contactEmail;
     private int id;
     private static int nextID = 1;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
         this.id = nextID;
+        this.contactEmail=contactEmail;
         nextID++;
+    }
+    public Event(){
+
     }
 
     public String getName() {
@@ -34,6 +48,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
