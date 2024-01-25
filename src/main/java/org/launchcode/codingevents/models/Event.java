716f88c4dbf70bfed470433815055b2bb40666a1
mbarcenas <misael.barcenas@yahoo.com>
2024-01-24 20:03:43 -0600

    completed validation exercises

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.Objects;
 
@@ -15,18 +13,31 @@ public class Event {
     @NotBlank
     @Email(message = "Invalid Email. Try again")
     private String contactEmail;
+
+    @NotBlank(message = "Need to include a location")
+    private String location;
+
+    @AssertTrue(message = "Must be able to attend")
+    private Boolean canAttend;
+
+    @Min(message = "Number of attendees must be one or more.", value = 0)
+    private int numberOfAttendees;
     private int id;
     private static int nextID = 1;
 
-    public Event(String name, String description, String contactEmail) {
+    public Event(String name, String description, String contactEmail, String location, Boolean canAttend, int numberOfAttendees) {
+        this();
         this.name = name;
         this.description = description;
-        this.id = nextID;
         this.contactEmail=contactEmail;
-        nextID++;
+        this.location = location;
+        this.canAttend = canAttend;
+        this.numberOfAttendees = numberOfAttendees;
+
     }
     public Event(){
-
+        this.id = nextID;
+        nextID++;
     }
 
     public String getName() {
@@ -56,6 +67,30 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getCanAttend() {
+        return canAttend;
+    }
+
+    public void setCanAttend(Boolean canAttend) {
+        this.canAttend = canAttend;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
     }
 
     @Override
