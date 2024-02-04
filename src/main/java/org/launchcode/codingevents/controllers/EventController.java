c89863a85caadddcaaa2bf95d9dd8d73b6de3273
mbarcenas <misael.barcenas@yahoo.com>
2024-02-04 16:32:21 -0600

    new branch for exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+
 
 
 @Controller
